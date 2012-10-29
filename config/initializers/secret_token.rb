d0bff28ca11badc0955b2d364f12c5f499cdf324
D. Falkner <dfalkner@surgeworks.com>
2012-10-28 22:33:08 -0600

    initial rails and heroku commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Loth::Application.config.secret_token = '070bb047e93a1a78260f7097b05bfe3abb19772945f1cc9b8087a5ee7a79f7b657f1a8ea4b63a50710d5428dce63d7a021655fcf2c58e35a7fc4f6ae12f7d64b'
