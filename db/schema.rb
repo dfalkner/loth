77f6512757950fd2569af5ab6b12e6e33f23475d
D. Falkner <dfalkner@surgeworks.com>
2012-11-07 09:17:30 -0700

    added signin, signout, and sessions controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121107054144) do
+ActiveRecord::Schema.define(:version => 20121107074047) do
 
   create_table "calendars", :force => true do |t|
     t.string   "label"
@@ -40,8 +40,10 @@ ActiveRecord::Schema.define(:version => 20121107054144) do
     t.string   "password_digest"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
